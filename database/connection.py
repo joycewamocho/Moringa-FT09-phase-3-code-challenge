17e029e83463d76840441913c4e1a4da52925492
joyce wamocho <joywamo@gmail.com>
2024-12-18 13:26:49 +0300

    stop usingsqlalchemy

--- database/connection.py
+++ database/connection.py
@@ -7,25 +7,6 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = "sqlite:///./database/magazine.db"  # Ensure this is the correct path
-
-# # Define the base class for models to inherit from
-# Base = declarative_base()
-
-# # Initialize the database connection
-# engine = create_engine(DATABASE_URL)
-
-# Session = sessionmaker(bind=engine)
-
-# session =Session()
-
-# # This function creates all tables
-# def init_db():
-#     Base.metadata.create_all(engine)
 
 
 
